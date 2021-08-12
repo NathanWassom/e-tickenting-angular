f00e125192a001048a85b07a4ac754da2d59e86d
kidriss <idriss.k@globalassetcameroon.net>
2021-08-12 10:13:15 +0100

    user service

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { User } from '../model/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<User[]> {
+    return this.http
+      .get<User[]>(environment.apiDomain + '/users')
+      .pipe(tap((t) => console.log(t)));
+  }
+}
